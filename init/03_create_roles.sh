7c6035ae1d89fe89b165b5d10bd947186951705a
Peter Stanko <pstanko@redhat.com>
2017-05-10 20:39:52 +0200

    fixed add user to user
    Signed-off-by: Peter Stanko <pstanko@redhat.com>

--- init/03_create_roles.sh
+++ init/03_create_roles.sh
@@ -7,15 +7,13 @@ while IFS= read -r var
 do
     if [ "${firts}" == "" ]; then
         first=$var
-    fi
-
-    if [ "${second}" == "" ]; then
+    elif [ "${second}" == ""]; then
         second=${var}
-        db_user_grant_user $first $second
     fi
 
     db_user_create $var
-    db_grant_user_table "customer" $var
-    db_grant_user_table "orders" $var
-    
+    db_user_grant_table "customer" $var
+    db_user_grant_table "orders" $var
 done < "$input"
+
+db_user_grant_user $first $second
