dcfe4eac068b8770db2c293401cec2149c3a6a6f
gevo20 <g.babcan@gmail.com>
2017-05-14 15:11:16 +0200

    Policies clean

--- scripts/sql/tests/simple/15_customer_delete_policies.sql
+++ scripts/sql/tests/simple/15_customer_delete_policies.sql
@@ -2,10 +2,10 @@
 
 SET ROLE tpcc;
 
-DROP POLICY select_customer ON customer;
-DROP POLICY update_customer ON customer;
-DROP POLICY insert_customer ON customer;
-DROP POLICY delete_customer ON customer;
+DROP POLICY IF EXISTS select_customer ON customer;
+DROP POLICY IF EXISTS update_customer ON customer;
+DROP POLICY IF EXISTS insert_customer ON customer;
+DROP POLICY IF EXISTS delete_customer ON customer;
 ALTER TABLE customer DISABLE ROW LEVEL SECURITY;
 
 SET ROLE tpcc;
